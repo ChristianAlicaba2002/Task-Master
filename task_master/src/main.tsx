26f4780f8e5fe56263e148b4913f1b98cec71faa
Christian <christiandave120702@gmail.com>
2025-08-05 22:51:54 +0800

    chore: modifided the mian file and add the routes also the publicRoute and protectedRoute

--- task_master/src/main.tsx
+++ task_master/src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
+import App from "./App.tsx"
+import Dashboard from "./pages/Dashboard.tsx"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ProtectedRoute, PublicRoute } from "./utils/AuthRoutes"
 
-createRoot(document.getElementById('root')!).render(
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <PublicRoute>
+        <App />
+      </PublicRoute>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <ProtectedRoute>
+      <Dashboard />
+      </ProtectedRoute>
+    )
+  },
+])
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={routes} />
+  </StrictMode>
 )
